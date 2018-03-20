f0cc1c3fd82ac0b85f20a6ef7ad7497cb5492d77
Benjamin Cash <cash.benj@gmail.com>
2018-03-20 09:23:24 -0600

    adding html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,22 @@
 var express = require("express");
 var router = express.Router();
+var path = require("path");
 
 //INSERT other HTML Routes Here - Attach to "router" instance to export//
+
+//GET route to load the landing-page from the ROOT directory//
+router.get("/", function(req, res) {
+    res.sendFile(path.join(_dirname, "../public/test.html")); //*UPDATE* with correct landing-page html
+});
+
+//GET route to load the event-specific pages//
+router.get("/event/:id", function(req, res) {
+    /*Here we will load the "event page" (html), and using handlebars on the front end fetch the specific event 
+    data from the DATABASE and send it back*/
+});
+
+//GET route to load the create-event page (no handlebars needed)//
+router.get("/create", function(req, res) {
+    res.sendFile(path.join(_dirname, "../public/test2.html")); //*UPDATE* with correct create-event html
+});
+
