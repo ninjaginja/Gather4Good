969af11613aad856c6c084ae97dde9e333b79616
Benjamin Cash <cash.benj@gmail.com>
2018-03-19 20:33:11 -0600

    adding models for user and event

---
+++ models/user.js
@@ -0,0 +1,25 @@
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        name: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(100),
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            // built into the user authentication? Hashed in database?
+        }
+    });
+
+    User.associate = function(models) {
+        User.hasMany(models.Event, {
+            onDelete: "cascade"
+        });
+    };
+
+    return User;
+}
