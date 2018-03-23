7befc689ddc0652bc579220250bb56bc314f9cb7
gjb117 <gregbrodzik@gmail.com>
2018-03-23 02:46:54 -0600

    adding functionality to display events associated with a particular cause. Functionality included in events.js and html-routes.js

--- public/assets/js/event.js
+++ public/assets/js/event.js
@@ -2,29 +2,61 @@ $(document).ready(function() {
 
   console.log(localStorage.getItem("token"));
 
+  // Retrieve data from form to create new event object
+  // To be called on click of form submit btn
+  function retrieveEventData() {
+    var newEvent = {
+          title: $("#event_name").val().trim(),
+          CauseId: $("#cause_category").val(),
+          date: $("#event_date").val().trim(),
+          time: $("#event_time").val().trim(),
+          description: $("#event_description").val().trim(),
+          img_url: $("#image_url").val().trim(),
+          location_name: $("#location_name").val().trim(),
+          location_street: $("#street").val().trim(),
+          location_city: $("#city").val().trim(),
+          location_state: $("#state").val().trim(),
+          location_zip: $("#zipcode").val().trim()
+        }
+
+    return newEvent;
+  }
+
+
+  // Function executing AJAX call to POST new event to Events table
+  // To be called onclick of form submit btn. Redirect user to newly-created
+  // event page if the submission to db was a success
+  function createNewEvent(newEventSettings) {
+    $.ajax(newEventSettings)
+      .done(function(response) {
+        console.log(response);
+        // console.log(response.id);
+        // $.get("/event/" + response.id, function(err) {
+        //   if(err) throw err;
+        // })
+      })
+      .fail(function(response) {
+        console.log(typeof response);
+        console.log(response);
+        console.log(response.responseJSON.message);
+
+        if(response.responseJSON.message === "Token expired") {
+          localStorage.removeItem("token");
+        }
+
+        $(".modal-trigger").show();
+        $("#error-msg").text("Please login before submitting your events");
+        $("#modal1").modal("open");
+      });
+  }
+
+  //On click event listener to create new event
   $("#new-event-btn").on("click", function(event) {
     event.preventDefault();
     var token = localStorage.getItem("token");
-
-    //Add validaiton logic here - e.g., if zip.length == 5 OK else return
+    var newEvent = retrieveEventData();
 
     console.log(token);
-
-    var newEvent = {
-      title: $("#event_name").val().trim(),
-      CauseId: $("#cause_category").val(),
-      date: $("#event_date").val().trim(),
-      time: $("#event_time").val().trim(),
-      // time: "2016-08-09 07:42:28",
-      description: $("#event_description").val().trim(),
-      img_url: $("#image_url").val().trim(),
-      location_name: $("#location_name").val().trim(),
-      location_street: $("#street").val().trim(),
-      location_city: $("#city").val().trim(),
-      location_state: $("#state").val().trim(),
-      location_zip: $("#zipcode").val().trim()
-    };
-
     console.log(newEvent);
 
     var newEventSettings = {
@@ -40,25 +72,22 @@ $(document).ready(function() {
         "data": newEvent
     }
 
-  $.ajax(newEventSettings)
-    .done(function(response) {
-      console.log(response);
-    })
-    .fail(function(response) {
-      console.log(typeof response);
-      console.log(response);
-      console.log(response.responseJSON.message);
-
-      if(response.responseJSON.message === "Token expired") {
-        localStorage.removeItem("token");
-      }
-
-      $(".modal-trigger").show();
-      $("#error-msg").text("Please login before submitting your events");
-      $("#modal1").modal("open");
-
-    });
+    createNewEvent(newEventSettings);
   });
 
+  //On click event listener to render display of events by specific cause
+  $(".causeBtn").on("click", function() {
+    var causeId = $(this).data("cause-id");
+    console.log("cause id: " + causeId);
+    displayEventsByCause(causeId);
+  });
+
+  function displayEventsByCause(causeId) {
+    if (causeId == "All") {
+      window.location.href = "/";
+    } else {
+      window.location.href = "/causes?cause_id=" + causeId;
+    }
+  }
 
 });
